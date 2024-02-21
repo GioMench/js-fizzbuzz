f9f97ea99517aee8a55f34b8d4ea81853c94f514
GioMench <giorgiaglm@gmail.com>
2024-02-21 19:01:23 +0100

    add class to li with js and add boostrap link to html

--- assest/js/main.js
+++ assest/js/main.js
@@ -9,24 +9,29 @@ for (let i = 1; i < 101; i++) {
         //console.log('fizzbuzz');
         const both = document.createElement('li');
         both.innerText = 'FizzBuzz';
+        both.setAttribute("class", "fizzbuzz")
         ulElement.append(both)
-        
+
 
     } else if (i % 3 === 0) {
         //console.log('fizz');
         const three = document.createElement('li');
         three.innerText = 'Fizz';
+        three.setAttribute("class", "fizz")
         ulElement.append(three)
 
     } else if (i % 5 === 0) {
         //console.log('buzz');
         const five = document.createElement('li');
         five.innerText = 'Buzz';
+        five.setAttribute("class", "buzz")
         ulElement.append(five)
 
     } else {
         const other = document.createElement('li');
         other.innerText = i ;
+        other.setAttribute("class", "other")
+
         ulElement.append(other)
     }
 
